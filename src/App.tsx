9b86d08caf111db68e6e8d58de9249ef109b3408
Harpreet Aulakh <hkaulakh14@gmail.com>
2021-04-08 11:49:57 -0500

    added chart,dashboard,dashheader,datatag components, trying to fix error in chart

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,57 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
-import NowWhat from './components/NowWhat';
+import Dashboard from './components/Dashboard';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
+import { onError } from 'apollo-link-error';
+
+
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+
+
+//websocket link
+const wsLink = new WebSocketLink({
+  uri: 'ws://react.eogresources.com/graphql',
+  options: {
+    //reconnect
+    reconnect: true,
+  },
+});
+
+//http request
+const httpLink = new HttpLink({
+  uri: 'https://react.eogresources.com/graphql',
+});
+
+let link = ApolloLink.from([
+  //error handling
+  onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+      );
+    }
+    if (networkError) console.error(`[Network error]: ${networkError}`, networkError.stack);
+  }),
+  ApolloLink.split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+  ),
+]);
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 const store = createStore();
 const theme = createMuiTheme({
@@ -28,11 +78,13 @@ const App = () => (
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
     <Provider store={store}>
-      <Wrapper>
-        <Header />
-        <NowWhat />
-        <ToastContainer />
-      </Wrapper>
+      <ApolloProvider client={client}>
+        <Wrapper>
+          <Header />
+          <Dashboard />
+          <ToastContainer />
+        </Wrapper>
+      </ApolloProvider>
     </Provider>
   </MuiThemeProvider>
 );
